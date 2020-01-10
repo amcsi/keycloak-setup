c21c0cf15fcad2005a6ae1bf447c34844c5ceabb
Attila Szeremi <attila@szeremi.org>
2020-01-10 11:07:13 +0100

    $fileName -> $archiveName

--- bin/download-keycload.sh
+++ bin/download-keycload.sh
@@ -6,4 +6,4 @@ source $(dirname "${0}")/variables
 # e.g. https://downloads.jboss.org/keycloak/8.0.0/keycloak-8.0.0.zip
 wget https://downloads.jboss.org/keycloak/$version/$archiveName
 
-unzip $filename
+unzip $archiveName
